b2c1dac82d56229fec4de9aa12f6f5a575d0e598
20013327 <20013327@myrp.edu.sg>
2021-06-08 10:17:37 +0800

    Done

--- app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayActivity.java
+++ app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.day2.demoarraysarraylist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class ArrayActivity extends AppCompatActivity {
@@ -17,8 +18,32 @@ public class ArrayActivity extends AppCompatActivity {
         tv = findViewById(R.id.textView1);
 
         //continue view the code in the worksheet
+        String [] array = new String [3];
+
+        array[0] = "Quick";
+        array[1] = "Brown";
+        array[2] = "Fox";
+
+        //remove
+        array[1] = null;
+
+        //add
+        array[1] = "Silver";
+
+        //update
+        array[1] = "Gold";
 
 
+        System.out.println("Size is : " + array.length);
 
+        String msg = "";
+        for (int i = 0; i < array.length; i ++){
+            msg += array[i] + "\n";
+        }
+
+        System.out.println(msg);
+        Log.d("Array", msg);
+
+        tv.setText(msg);
     }
 }
--- app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
+++ app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class ArrayListActivity extends AppCompatActivity {
 
     TextView tv;
@@ -17,7 +19,32 @@ public class ArrayListActivity extends AppCompatActivity {
         tv = findViewById(R.id.textView2);
 
         //continue view the code in the worksheet
+        ArrayList<String> al = new ArrayList<String>();
 
+        al.add("Quick");
+        al.add("Brown");
+        al.add("Fox");
+
+        //remove
+        al.remove("Brown");
+
+        //add
+        al.add(1, "Silver");
+
+        //update
+        al.set(1, "Gold");
+
+        System.out.println("Size is : " + al.size());
+
+        String text = "Fruits\n";
+        text += "=====\n";
+
+
+        for (int i = 0; i < al.size(); i ++){
+            text += al.get(i) + "\n";
+        }
+
+        tv.setText(text);
 
     }
 }
